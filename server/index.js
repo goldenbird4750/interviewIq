7fd81ce3d32bc941dd08f23194302462cfc488ee
goldenbird <goldenbird4750@gmail.com>
2026-04-05 12:39:34 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express()
+
+
+const PORT = process.env.PORT || 6000 
+
+app.get("/",(req,res)=>{
+  return res.json({message:"server started"})
+})
+
+app.listen(PORT,()=>{
+  console.log(`server running on port ${PORT}`)
+})
+
